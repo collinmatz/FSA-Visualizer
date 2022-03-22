34cf1050c85012b8b961ad926cd8dfeb23de034a
Matz <cam210003@utdallas.edu>
2022-03-22 15:51:12 -0500

    started valid check for regex

--- FSA-V.js
+++ FSA-V.js
@@ -1,5 +1,30 @@
+let groups = [];
+
 function genFSA(regex) {
-    for (let i = 0; i < regex.length; i++) {
-        console.log(regex[i].charCodeAt(0))
+    if (checkForValidRegex(regex))
+        console.log(groups);
+    else
+        document.getElementById("err_msg").innerHTML = "Error. Please enter a valid regular expression."
+}
+
+function checkForValidRegex(input) {
+    let stack = [];
+
+    let i = 0;
+    while (i < input.length) {
+        if (input[i] == '(' || input[i] == '[' || input[i] == '{') {
+            let newGroup = []; // new regex grouping starting with opening symbol
+            while (i < input.length && input[i] != ')') {
+                stack.push(input[i]);
+                i++;
+            }
+            if (i > input.length)
+                return false;
+            while (stack.length > 0)
+                newGroup.push(stack.pop());
+            groups.push(newGroup);
+        }
+        i++;
     }
+    return true;
 }
